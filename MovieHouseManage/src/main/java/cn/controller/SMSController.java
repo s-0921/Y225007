bf1492f770e3550153821c5efab663186d819d33
fxy <18937123228@163.com>
2019-12-17 13:32:29 +0800

    新的带短信验证功能位实现

---
+++ MovieHouseManage/src/main/java/cn/controller/SMSController.java
@@ -0,0 +1,13 @@
+package cn.controller;
+
+import cn.utils.YunPianTool;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SMSController {
+
+}
