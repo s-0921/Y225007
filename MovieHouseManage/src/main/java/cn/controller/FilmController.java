fde394e8a6e8f55c309b90627ac79a534794c721
cuican <cc001104@163.com>
2019-12-18 16:37:53 +0800

    修改了FilmController

--- MovieHouseManage/src/main/java/cn/controller/FilmController.java
+++ MovieHouseManage/src/main/java/cn/controller/FilmController.java
@@ -9,11 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
