82499652c395cdbda83f3510520c96bbd0e17f2b
cuican <cc001104@163.com>
2019-12-19 15:33:13 +0800

    FilmImgMapper添加功能

--- MovieHouseManage/src/main/java/cn/mapper/FilmImgMapper.java
+++ MovieHouseManage/src/main/java/cn/mapper/FilmImgMapper.java
@@ -2,8 +2,16 @@ package cn.mapper;
 
 import cn.entity.FilmImg;
 
-public interface FilmImgMapper {
-    public int insert(FilmImg record);
+import java.util.List;
 
-    public int insertSelective(FilmImg record);
+public interface FilmImgMapper {
+    public void insert(FilmImg record);
+
+    public void insertSelective(FilmImg record);
+
+    //根据id查询电影图集
+    public List<FilmImg> selectByFilmId(Integer fId);
+
+    //删除图片
+    public void deleteById(Integer id);
 }
