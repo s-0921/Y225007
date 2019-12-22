1fa70e357a0fa99a9677fc4538390681c568d40a
sl <sbn725112@163.com>
2019-12-22 16:42:48 +0800

    选择电影 选择影院 登录 主页面

--- MovieHouseManage/src/main/java/cn/controller/TimerTask.java
+++ MovieHouseManage/src/main/java/cn/controller/TimerTask.java
@@ -1,7 +1,6 @@
 package cn.controller;
 
 import cn.entity.Film;
-import org.springframework.http.HttpRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class TimerTask {
 
     //每隔5秒  0/5 * * * * ?
     //每隔一小时 0 0 */1 * * ?
-    @Scheduled(cron="0/5 * * * * ?")
+    //@Scheduled(cron="0/5 * * * * ?")
     public void test1(HttpServletRequest request){
 
         //System.out.println("定时执行任务");
